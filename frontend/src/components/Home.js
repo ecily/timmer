02c5ba695b58a218f9ab080ac4b684f4f3d0502b
Andreas Franz <andreas.franz@ecily.com>
2021-11-05 14:55:38 +0100

    push

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -9,6 +9,9 @@ import Pagination from 'react-js-pagination'
 import Slider from 'rc-slider'
 import 'rc-slider/assets/index.css'
 
+
+
+
 const { createSliderWithTooltip } = Slider
 const Range = createSliderWithTooltip(Slider.Range)
 
@@ -61,123 +64,164 @@ const Home = ({ match }) => {
     return (
         <Fragment>
 
-            {loading ? <Loader/> : (
-                <Fragment>
-                <MetaData title={'ecily/Webdevelopment showcase'}/>
+        <a href="#" className="scrollToTop"><i className="icofont-rounded-up"></i></a>
 
-                <h1 id="products_heading">Unsere Produkte</h1>
-                <section id="products" className="container mt-5">
-                    <div className="row">
-
-                        {keyword ? (
-                            <Fragment>
-                                <div className='col-6 col-md-3 mt-5 mb-5'>
-                                    <div className='px-5'>
-                                        <Range
-                                            marks={{
-                                                1: `EUR 1`,
-                                                1000: `EUR 200`
-                                            }}
-                                            min={1}
-                                            max={200}
-                                            defaultValue={[1, 1000]}
-                                            tipFormatter={value => `${value}`}
-                                            tipProps={{
-                                                placement: 'top',
-                                                visible: true
-                                            }}
-                                            value={price}
-                                            onChange={price => setPrice(price)}
-                                            />
-
-                                        <hr className='my-5' />
-                                        <div className='mt-5'>
-                                            <h4 className='mb-3'>
-                                                Categories
-                                            </h4>
-                                            <ul className='pl-0'>
-                                                {categories.map(category => (
-                                                <li
-                                                    style={{
-                                                        cursor: 'pointer',
-                                                        listStyleType: 'none'
-                                                    }}
-                                                    key={category}
-                                                    onClick={() => setCategory(category)
-                                                    }
-                                                >
-                                                {category} 
-                                                </li>
-                                                ))}
-                                            </ul>
-                                        </div>
-                                        <hr className='my-3' />
-                                        <div className='mt-5'>
-                                            <h4 className='mb-3'>
-                                                Bewertungen
-                                            </h4>
-                                            <ul className='pl-0'>
-                                                {[5,4,3,2,1].map(star => (
-                                                <li
-                                                    style={{
-                                                        cursor: 'pointer',
-                                                        listStyleType: 'none'
-                                                    }}
-                                                    key={star}
-                                                    onClick={() => setRating(star)
-                                                    }
-                                                >
-                                                <div className='rating-outer'>
-                                                    <div className='rating-inner'
-                                                        style={{
-                                                            width: `${star * 20}%`
-                                                        }}
-                                                    >
-
-                                                    </div>
-                                                </div>
-                                                </li>
-                                                ))}
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className='col-6 col-md-9'>
-                                    <div className='row'>
-                                        {products && products.map(product => (
-                                        <Product key={product._id} product={product} col={5}/>
-                                        ))}
-                                    </div>
-                                </div>
-                            </Fragment>
-                        ) : (
-                        products && products.map(product => (
-                        <Product key={product._id} product={product} col={3}/>
-                        ))
-                        )}
-                        
-                       
+	    <div className="bg-theme position-relative">
+		<div className="shape-top-left  d-xxl-block wow fadeInDown" data-wow-delay="0.4s">
+            <img src='/images/shape/01.png'/>
+		</div>
+            <div className="shape-bottom-right d-none d-xxl-block wow fadeInUp" data-wow-delay="0.4s">
+            <img src='/images/shape/02.png'/>
+            </div>
+            <div className="shape-bottom wow fadeInUp" data-wow-delay="0.4s">
+            <img src='/images/shape/03.png'/>
+            </div>
+            <div className="container">
+                <div className="row align-items-center position-relative z-index-1">
+                    <div className="col-lg-6 col-12 wow fadeInLeft" data-wow-delay="0.4s">
+                        <div className="banner-content">
+                            <span className="banner-sub-title text-white">Timmer's Nordmann-Tannen</span>
+                            <h2 className="banner-title">Frohe Weihnachten</h2>
+                            <p className="banner-desc text-white">Die Nordmann-Tanne (Abies nordmanniana), 
+                            standardsprachlich Nordmanntanne, auch Nordmanns Tanne und Kaukasus-Tanne genannt, 
+                            ist eine Pflanzenart aus der Gattung Tannen (Abies) in der Familie der 
+                            Kieferngewächse (Pinaceae). Die Nordmanntanne ist heute die meistgenutzte Baumart 
+                            als Weihnachtsbaum.</p>
+                            <div className="banner-button-group d-inline-flex">
+                                <a href="#pricing" className="lab-btn"><span>Jetzt einkaufen!</span></a>
+                            </div>
+                        </div>
                     </div>
-                    </section>
-                    {resPerPage <= count && (
-                    
-                    <div className='d-flex justify-content-center mt-5'>
-                        <Pagination
-                            activePage={currentPage}
-                            itemsCountPerPage={resPerPage}
-                            totalItemsCount={productsCount}
-                            onChange={setCurrentPageNo} 
-                            nextPageText={'Next'}
-                            prevPageText={'Previous'}
-                            firstPageText={'First'}
-                            lastPageText={'Last'}
-                            itemClass='page-item'
-                            linkClass='page-link'
-                            />
+                    <div className="col-lg-6 col-12 mt-5 mt-lg-0 wow fadeInRight" data-wow-delay="0.4s">
+                        <div className="banner-thumb">
+                            <img src="/images/banner/01.png" alt="ChampEvent"/>
+                        </div>
                     </div>
-                    )}
-                </Fragment>)}
-    </Fragment>
+                </div>
+            </div>
+	</div>
+
+    {/* Product Section REACT */}
+
+	
+	
+    {/* <!-- ==========Pricing Section Starts Here========== --> */}
+	<div className="pricing-section padding-top padding-bottom position-relative" id="pricing">
+		<div className="shape-top-right d-xl-block wow fadeInDown" data-wow-delay="0.4s">
+			<img src="/images/shape/09.png" alt="ChampEvent"/>
+		</div>
+		<div className="shape-bottom wow fadeInUp" data-wow-delay="0.8s">
+			<img src="/images/shape/08.png" alt="ChampEvent"/>
+		</div>
+		<div className="container">
+			<div className="section-header text-center wow fadeInUp" data-wow-delay="0.4s">
+				<span className="section-subtitle theme-color">bis 20. Dezember bestellen und rechtzeitig zum Fest geliefert bekommen</span>
+				<h2 className="section-title">unsere Nordmann-Tannen</h2>
+			</div>
+			<div className="section-wrapper">
+		
+
+				{/* <!-- Nordmanntanne hier --> */}
+				<div className="col-12">
+					<div className="section-wrapper text-center">
+						<div className="footer-logo wow fadeInUp" data-wow-delay="0.4s">
+							<img src="/images/pricing/tree.png" alt="ChampEvent"/>
+						</div>
+					</div>
+				</div>
+
+
+<div className="row justify-content-center align-items-center g-4">
+	
+				{products && products.map(product => (
+                <Product key={product._id} product={product} col={3}/>
+				))}
+			
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+    {/* <!-- ==========About Section Starts Here========== --> */}
+	<div className="about-section padding-top padding-bottom position-relative" id="about">
+		{/* <div className="shape-bottom-right d-none d-xxl-block wow fadeInUp" data-wow-delay="0.8s">
+			
+            <img src="/images/shape/02.png" alt="ChampEvent"/>
+		</div> */}
+
+        <div className="shape-bottom-right d-xxl-block wow fadeInUp" data-wow-delay="0.8s">
+			<img src="/images/shape/05.png" alt="ChampEvent"/>
+		</div>
+		<div className="container">
+			<div className="row align-items-center justify-content-center position-relative z-index-1">
+				<div className="col-lg-6 col-12 wow fadeInLeft" data-wow-delay="0.4s">
+					<div className="about-thumb ml-xxl-450">
+						<img src="/images/about/01.jpg" alt="ChampEvent" className="w-xxl-none"/>
+					</div>
+				</div>
+				<div className="col-lg-6 col-12 wow fadeInUp" data-wow-delay="0.4s">
+					<div className="about-content">
+						<span className="about-subtitle theme-color">Über uns</span>
+						<h2 className="about-title">Timmer's Nordmann-Tannen</h2>
+						<p className="about-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum harum alias vero, quos error quia ea eum tempora ex similique reiciendis, dolore temporibus, suscipit maxime asperiores rerum molestiae! Voluptatum, quod.</p>
+						<p className="about-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ut natus voluptatem vero architecto quas rerum ad sit expedita est repellat provident maxime distinctio aliquid totam molestiae eum, quae doloribus.</p>
+						<ul className="about-list">
+							<li><i className="icofont-checked"></i> We providing different.</li>
+							<li><i className="icofont-checked"></i> We are one of leading.</li>
+							<li><i className="icofont-checked"></i> Learn how to grow you.</li>
+							<li><i className="icofont-checked"></i> We providing different.</li>
+							<li><i className="icofont-checked"></i> Learn how to grow you.</li>
+							<li><i className="icofont-checked"></i> We are one of leading.</li>
+						</ul>
+						<a href="#pricing" className="lab-btn"><span>Unsere Tannen</span></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	{/* <!-- ==========About Section Ends Here========== --> */}
+
+    {/* <!-- ==========Newsletter Section Starts Here========== --> */}
+	<footer className="footer-section position-relative bg-theme">
+		<div className="shape-top wow fadeInDown" data-wow-delay="0.4s">
+			<img src="/images/shape/09.png" alt="ChampEvent"/>
+		</div>
+		<div className="shape-bottom wow fadeInUp" data-wow-delay="0.8s">
+			<img src="/images/shape/08.png" alt="ChampEvent"/>
+		</div>
+		<div className="container">
+			<div className="row">
+				<div className="col-12">
+					<div className="section-wrapper text-center">
+						<div className="footer-logo wow fadeInUp" data-wow-delay="0.4s">
+							<img src="/images/logo/Wiesenseppl.png" alt="ChampEvent"/>
+						</div>
+						<br/>
+						<div className="footer-copyright wow fadeInUp" data-wow-delay="0.6s">
+							<p className="mb-0 text-white">Firmenname - Impressum & Datenschutz - Cookiehinweis</p>
+							<p className="mb-0 text-white">Kontaktadresse</p>
+						</div>
+
+						<div className="footer-social wow fadeInUp" data-wow-delay="0.5s">
+							<div className="social-media">
+								<a href="#" className="facebook"><i className="icofont-facebook"></i></a>
+								<a href="#" className="twitter"><i className="icofont-twitter"></i></a>
+								<a href="#" className="linkedin"><i className="icofont-linkedin"></i></a>
+								<a href="#" className="youtube"><i className="icofont-youtube-play"></i></a>
+							</div>
+						</div>
+					
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	{/* <!-- ==========Newsletter Section Ends Here========== --> */}
+               
+        </Fragment>
     )}
+
+
 
 export default Home
--- frontend/src/components/product/Product.js
+++ frontend/src/components/product/Product.js
@@ -3,29 +3,33 @@ import { Link } from 'react-router-dom'
 
 const Product = ({ product, col }) => {
     return (
-        <div className={`col-sm-12 col-md-6 col-lg-${col} my-3`}>
-                              
-        <div className="card p-3 rounded">
-            <img
-            className="card-img-top mx-auto"
-            src={product.images[0].url}
-            alt={product.name}/>
-            <div className="card-body d-flex flex-column">
-            <h5 className="card-title">
-                <Link to={`/product/${product._id}`}>{product.name}</Link>
-            </h5>
-            <div className="ratings mt-auto">
-                <div className="rating-outer">
-                <div className="rating-inner" 
-                style={{ width: `${(product.ratings / 5) * 100}%` }}></div>
-                </div>
-                <span id="no_of_reviews">({product.numOfReviews} Reviews)</span>
-            </div>
-            <p className="card-text">EUR {product.price}</p>
-            <Link to={`/product/${product._id}`} id="view_btn" className="btn btn-block">Details</Link>
-            </div>
+        <div className="row justify-content-center align-items-center g-4">   
+        <div className="fadeInUp" data-wow-delay="0.4s">
+     
+        <div className="pricing-item">
+		<div className="pricing-inner text-center">
+          
+                    <div className="pricing-content">
+                        <h3 className="pricing-title">
+                            {product.name}
+                        </h3>
+
+                        {/* <div className="ratings mt-auto">
+                            <div className="rating-outer">
+                            <div className="rating-inner" 
+                            style={{ width: `${(product.ratings / 5) * 100}%` }}></div>
+                            </div>
+                            <span id="no_of_reviews">({product.numOfReviews} Reviews)</span>
+                        </div> */}
+                        <p className="pricing-price">EUR {product.price}</p>
+                        <Link to={`/product/${product._id}`}  className="lab-btn">Details</Link>
+                    </div>
         </div>
-        </div> 
+        </div>
+        </div>
+        </div>
+        
+        
     )
 }
 
